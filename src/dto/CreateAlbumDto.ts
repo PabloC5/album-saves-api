a87a9fdfd0d0b18404d2c7c746f04688e00c8c65
pablo correia <pablocorreia2012@gmail.com>
2025-02-15 20:23:45 -0300

    feito os ajsutes que faltavam

--- src/dto/CreateAlbumDto.ts
+++ src/dto/CreateAlbumDto.ts
@@ -2,8 +2,8 @@ import {IsString} from 'class-validator';
 
 export class CreateAlbumDto {
     @IsString({ message: 'O nome do album deve ser uma string válida.' })
-    name_album: string;
+    name: string;
 
     @IsString({ message: 'A url deve ser uma string válida.' })
-    album_image: string;
+    url_image: string;
 }
